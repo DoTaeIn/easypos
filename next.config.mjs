683965ecda907fba13f070801d333c97d4550f82
DoTaeIn <hirobin0412@gmail.com>
2024-09-26 02:47:21 +0900

    Build

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true, // Disable ESLint during build
-    },
-    
-};
-
-export default nextConfig;
